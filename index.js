1f44a32ab3327ca80ee67e81ba7a909481c4c1fa
AliShabab62 <Alishabab62@gmail.com>
2022-11-23 17:06:11 +0530

    Complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const invalid=document.getElementById('invalid');
 const input=document.getElementById('input')
 const output=document.getElementById('output');
 function random(){
+    input.value=""
     var cap=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
     var a=cap[Math.floor(Math.random()*62)];
     var b=cap[Math.floor(Math.random()*62)];
@@ -17,7 +18,13 @@ function random(){
     
     sub.addEventListener('click', ()=>{
         if(input.value != output.value){
-        invalid.innerText="Invalid Captcha!!!"
+            // invalid.innerText="Invalid Captcha!!!"
+            alert("Incorrect Captcha!!!");
+            input.value=""
+    }
+    else{
+        invalid.innerText="Your Captcha Is Correct👍";
+        input.value=""
     }
     });
     
